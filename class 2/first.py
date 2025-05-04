1f991d583797820acb5c3ad0977d0689b2377c76
abdullahansarigit <abdullahbusiness11@gmail.com>
2025-05-04 22:01:29 +0500

    first

---
+++ class 2/first.py
@@ -0,0 +1,20 @@
+print("pakistan")
+
+
+first ="20"
+
+second = "abc"
+
+print(first + second)
+
+
+name = "first"
+
+print(name)
+
+a = input('Enter a first number')
+print(a)
+
+b = input('Enter a second number')
+c = a  + b
+print(c)
