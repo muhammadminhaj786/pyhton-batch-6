8265eacd8956eb02938d428fe6064651cec09f85
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-05-11 21:50:58 +0500

    3

---
+++ class 3/third.py
@@ -0,0 +1,33 @@
+#airthematic operator
+
+# num1 = input('Enter a first number')  #4
+# num2 = int(num1)     #4
+# num5 = str(num1)
+# num3 = int(input('Enter a second number'))
+# num4 = int(num3)
+# print(num5 * num3)
+
+#str * int legal 
+#str + str legal 
+#int + int legal 
+#int + str il legal 
+#int / str illegal
+#str / str illegal
+
+name =input('Enter a name')
+age = input("Enter your age")
+print("hello" ,name, "you are ",age, "old")
+
+
+
+#assignment operator 
+# = +=
+# num6 = 9
+# num6 += 1
+
+num1 = input('enter first number') #6
+num2 = int(num1)  #6 
+num3 = num2 + 1  #7
+# num3 = num3 + 1
+num3+=1
+print(num3)
