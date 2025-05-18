ea745cfb09a01cf3364769253fa31134b4338197
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-05-18 21:47:09 +0500

    condition

---
+++ New folder/third.py
@@ -0,0 +1,52 @@
+# hello = "Hello"
+# print(hello)
+
+
+# num1 = int(input('Enter first number'))  #16
+# num2 = int(input('Enter Second number'))  #3
+
+# num3 = str(num1)
+
+
+# print(num1 + num2)
+# print(num1 -num2)
+# print(num1 / num2)
+# print(num1 * num2)
+# print(num1 % num2)
+
+
+# num1 = num1 + 1
+# print(num1 )  #
+
+
+#comparsion operator  > < >= <= == !=
+
+# a = 5
+# b = 6
+# print(a == b)
+# print(a != 8)
+# print(b >= a)
+# print(a < b)
+
+
+# condition
+# abc = 'abc'
+# dfc = 'ab'
+# print(dfc == abc)
+
+# if condition:
+#     print('true')
+
+# rollNo = 123
+# userRole = (input('Enter your roll number'))
+# dummy = False
+
+
+# if dummy == False: 
+#     print('allowed')
+# else:
+#     print('not allowed')
+
+
+# if 'abc'=='ABC':
+#     print('equal')
