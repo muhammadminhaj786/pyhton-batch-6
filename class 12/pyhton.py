f15514a12f486cf91c7ff3baae37d0cd2b57887d
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-07-31 15:34:52 +0500

    oop

---
+++ class 12/pyhton.py
@@ -0,0 +1,99 @@
+
+
+# a1 = ['minhaj', 'ali','raza']
+# a1.remove(2)
+# print(a1)
+
+
+
+#school  management
+# students = []
+
+# std1 = {
+#     "name": "minhaj",
+#     "age": 23,
+#     "rollno": "123"
+# }
+
+# std2 = {
+#     "name": "ali",
+#     "age": 22,
+#     "rollNo": "345"
+    
+# }
+
+# std = {
+#     name,
+#     age,
+#     rollNo
+# }
+
+# std2.pop("rollNo")
+# students.append(std1)
+# students.append(std2)
+# print(len(students))
+
+#oop
+#object oriented programming
+# classes
+# constructor init function
+# attributes
+#self => current refrence of object 
+# method= > function belongs to object
+
+# class students:
+#     #class attributes
+#     name = 'minhaj'
+#     def __init__(self,name,age):
+#         #object attributes
+#         self.name = name
+#         self.age = age
+#     def show_info (self):
+#         print(self.name)
+
+# # std1 = students("minhaj", 23)
+# std2 = students('ali',23)
+
+# print(std2.name)
+# # print(std1.show_info())
+
+# class Car:
+#     def __init__(self,color):
+#         self.color = color
+#     def start(self):
+#         print('car start')
+
+
+# car1 = Car("red")
+# car2 = Car("green")
+
+# print(car1.color)
+# # print(car2.start())
+# car2.start()
+
+
+
+# class Student:
+#     def __init__(self, firstName,lastName,age):
+#         self.firstName = firstName,
+#         self.lastName = lastName,
+#         self.age = age,
+
+#     def fullName(self):
+#         print(f"{self.firstName} {self.lastName}")
+#     # def totalMarks(self):
+#     #     total = 0
+#     #     for mark in self.marks:
+#     #        print(mark)
+
+#     def allowed(self):
+#         print(self.age)
+#         # if age > 18:
+#         #     print('allowed')
+#         # else: 
+#         #     print('not allowed')
+        
+       
+
+# std1 = Student('minhaj','wahid',"23")
+# std1.allowed()
