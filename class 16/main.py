b7c23bf1ad0ab9302e025cf010c30935f37d1a81
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-09-07 21:07:54 +0500

    exception

---
+++ class 16/main.py
@@ -0,0 +1,41 @@
+# modular
+#first method
+# import mymodule 
+# print(mymodule.add(5,5))
+
+
+#second method
+ # from mymodule import add
+# print(add(5,5))
+
+
+#third method
+# # import mymodule as z
+# print(z.add(4,7))
+
+
+
+#exception try 
+
+
+
+# def myApp():
+#     num = int(input("Enter a number: "))
+#     if num == 0:
+#         print('"Zero not allowed')
+#         return
+#     print(10/num)
+
+# myApp()
+
+# try:
+#     num = int(input("Enter a number: "))
+#     print(10/num)
+# except ZeroDivisionError:
+#     print("zero is not divisible")
+# except ValueError:
+#     print('num is not divsible by string')
+# finally:
+#     print('bye')
+
+# print('sab shi he ')
