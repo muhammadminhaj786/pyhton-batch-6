88cd111bccf4b567ef68d170bbc9953be52008f6
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-09-21 21:22:57 +0500

    last class

--- class 17/main.py
+++ class 17/main.py
@@ -3,3 +3,6 @@ import module as md
 
 print("hello")
 print(md.add(3,4))
+
+
+a = 1,2,3,4,5,6,7,8,9,10
---
+++ class 18/revision.py
@@ -0,0 +1,66 @@
+
+
+name = 123
+
+names = [
+    'a','b','c'
+]
+
+user = {
+    name :'a'
+}
+
+names[2] = 'd'
+names.append('e')
+# names.remove('e')
+# names.pop()
+
+
+
+print(names)
+
+
+
+# print(num[0])
+# print(num[1])
+# print(num[2])
+# print(len(num))
+
+# for i in num:
+#     print(i)
+
+
+
+# for i in range(1,11):
+#     print(i)
+
+# num = [1,2,3,4,5,6,7]
+# add = 0
+# for i in num:
+#     # add = add + i
+#     add+=i
+# print(add)
+
+# user_inp = int(input('Enter a number'))
+
+# if (user_inp == 2):
+#     print('yes')
+# else:
+#     print('no')
+
+# user_age = int(input('Enter your age'))
+
+# if (user_age >= 18):
+#     print('allowed')
+# else:
+#     print('not allowed')
+
+# def repeat(a,b):
+#     for i in range(a,b):
+#         print(i)
+
+# repeat(11,20)
+
+# repeat(12,30)
+
+
